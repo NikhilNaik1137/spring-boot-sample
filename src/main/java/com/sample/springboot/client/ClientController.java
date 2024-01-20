edae15a241fa62f55e3ae7db652cc8bb67f053d8
Nikhil Naik <nikhilnaik1137@gmail.com>
2024-01-20 23:46:33 +0530

    API URL modification

--- src/main/java/com/sample/springboot/client/ClientController.java
+++ src/main/java/com/sample/springboot/client/ClientController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("api/v1/clients/")
+@RequestMapping("api/v1/clients")
 public class ClientController {
 
     private final ClientService clientService;
